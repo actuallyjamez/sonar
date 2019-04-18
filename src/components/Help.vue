47a1b450f645bcb76f8c2188008130724c42c435
James Morris <actuallyjamez@gmail.com>
2019-04-18 23:05:18 +0100

    removed ring from help view

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -1,22 +1,5 @@
 <template>
     <div id="help" class="bezier" :class="{ hidden: hidden }">
-        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-             height="64" width="64">
-            <defs>
-                <ellipse id="ellipse-1" opacity="1" rx="32" ry="32" cx="32" cy="32"></ellipse>
-                <mask id="mask-2" maskUnits="userSpaceOnUse" maskContentUnits="userSpaceOnUse" x="0"
-                      y="0">
-                    <rect opacity="1" x="0" y="0" width="64" height="64" fill="black"></rect>
-                    <use xlink:href="#ellipse-1" fill="white"></use>
-                </mask>
-            </defs>
-            <g opacity="1">
-                <use xlink:href="#ellipse-1" fill-opacity="0" stroke="rgb(255,255,255)"
-                     stroke-opacity="1"
-                     stroke-width="20" stroke-linecap="butt" stroke-linejoin="miter"
-                     mask="url(#mask-2)"></use>
-            </g>
-        </svg>
         <h1>Select "Sonar Player" from Available Devices in the Spotify app.</h1>
     </div>
 </template>
