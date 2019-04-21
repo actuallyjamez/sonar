f741cf789e1c6cec11fc49fa4cc85606f98e182a
James Morris <actuallyjamez@gmail.com>
2019-04-21 16:01:29 +0100

    updated help font size

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -29,7 +29,7 @@
         position: absolute;
         z-index: 3;
         font-weight: bold;
-        font-size: 0.9em;
+        font-size: 1.6em;
         left: 50%;
         top: 50%;
         transform: translate(-50%, -50%);
